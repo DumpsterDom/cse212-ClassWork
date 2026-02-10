2f09e8b724a243ccc2c65b1b147743ce6a0bcea0
DumpsterDom <dominictg926@gmail.com>
2026-02-10 16:55:56 -0500

    Trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -13,6 +13,7 @@ public class Node
     {
         // TODO Start Problem 1
 
+        // sskip insert  so dupes will be ignored 
         if (value < Data)
         {
             // Insert to the left
@@ -21,7 +22,7 @@ public class Node
             else
                 Left.Insert(value);
         }
-        else
+        else if (value > Data)
         {
             // Insert to the right
             if (Right is null)
@@ -34,12 +35,31 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+
+        // Found the value and if not cheeck Left if smaller and Right if bigger 
+        if (value == Data)
+        {
+            return true; 
+        }
+        else if (value < Data)
+        {
+            return Left != null && Left.Contains(value);
+        }
+        else
+        {
+            return Right != null && Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (this == null)
+            return 0;
+
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
